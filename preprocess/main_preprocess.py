9ebae4943ae6a51f37e3ad863a9b724f012aa62b
guanghuifu-pc <aslanfu123@gmail.com>
2024-02-07 12:32:26 +0100

    data processing

--- preprocess/main_preprocess.py
+++ preprocess/main_preprocess.py
@@ -1,6 +1,7 @@
 import os
-import data_split
-import binary_label
+import preprocess.data_split as data_split
+import preprocess.binary_label as binary_label
+import preprocess.select_t1gd as select_t1gd
 
 data_split_csv_path = '../brats-data_split.csv'
 images_all_path = 'your/dataset/path/Task01_BrainTumour/imagesTr'
@@ -14,8 +15,14 @@ os.makedirs(labels_tr_path, exist_ok=True)
 os.makedirs(images_ts_path, exist_ok=True)
 os.makedirs(labels_ts_path, exist_ok=True)
 
+# Pre-step: download the dataset from medical segmentation decathlon website: https://drive.google.com/file/d/1A2IU8Sgea1h3fYLpYtFb2v7NYdMjvEhU/view?usp=drive_link
+
 # 1. Split dataset based on the csv file
 data_split.main(images_all_path, labels_all_path, images_tr_path, labels_tr_path, images_ts_path, labels_ts_path, data_split_csv_path)
+
+# 2. Get T1-GD MRI from MSD-BraTs dataset
+select_t1gd.main(images_tr_path, images_tr_path)
+select_t1gd.main(images_ts_path, images_ts_path)
 
 # 2. Binary label. The enhancing tumor is selected as the segmentation target
 binary_label.main(labels_tr_path, labels_tr_path)
---
+++ preprocess/select_t1gd.py
@@ -0,0 +1,29 @@
+import os
+import glob
+import nibabel as nib
+
+
+def load_and_save_t1gd_nifti(folder_path, output_folder):
+    # Define the modality of interest
+    modality_index = 2  # T1gd is the third modality (0-indexed)
+    # Process each file
+    for file_path in glob.glob(os.path.join(folder_path, '*.nii.gz')):
+        img = nib.load(file_path)
+        data = img.get_fdata()
+
+        # Check if the image has the expected shape
+        if data.shape != (240, 240, 155, 4):
+            print(f"Skipping {os.path.basename(file_path)}, unexpected shape: {data.shape}")
+            continue
+
+        # Extract and save only the T1gd modality data
+        modality_data = data[:, :, :, modality_index]
+        modality_img = nib.Nifti1Image(modality_data, img.affine)
+        modality_file_path = os.path.join(output_folder, os.path.basename(file_path))
+        nib.save(modality_img, modality_file_path)
+
+    print("Processing complete.")
+
+
+def main(folder_path, output_folder):
+    load_and_save_t1gd_nifti(folder_path, output_folder)
